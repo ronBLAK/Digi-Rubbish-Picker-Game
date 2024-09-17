ae08c621a5a10b33cdd832492dd837c23d3503bd
ronBLAK <159859853+ronBLAK@users.noreply.github.com>
2024-09-17 19:13:13 +1200

    Update InstructionMenuTextSetter.cs

--- Scripts/InstructionMenuTextSetter.cs
+++ Scripts/InstructionMenuTextSetter.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using TMPro;
+using TMPro; // Namespace for TextMeshPro components
 using UnityEngine;
 
 public class InstructionMenuTextSetter : MonoBehaviour
 {
+    // Reference to the instruction menu panel GameObject
     [SerializeField] public GameObject instructionMenuPanel;
-    private TextMeshProUGUI instructions;
+    private TextMeshProUGUI instructions; // Reference to the TextMeshProUGUI component for setting text
 
     void Start()
     {
+        // Find the TextMeshProUGUI component within the children of the instruction menu panel
         instructions = instructionMenuPanel.GetComponentInChildren<TextMeshProUGUI>();
 
+        // Set the initial text for the instructions
         instructions.text = "hello ";
     }
 }
