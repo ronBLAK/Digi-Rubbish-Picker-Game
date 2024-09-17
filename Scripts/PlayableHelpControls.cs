d85d4ac623238e9852321276f862c51b75dcc1fb
ronBLAK <159859853+ronBLAK@users.noreply.github.com>
2024-09-17 19:20:00 +1200

    Update PlayableHelpControls.cs

--- Scripts/PlayableHelpControls.cs
+++ Scripts/PlayableHelpControls.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
+using System.Threading; // Namespace for threading (not used in this script)
 using UnityEngine;
 
 public class PlayableHelpControls : MonoBehaviour
 {
+    // Reference to the GameObject containing the help controls instruction panel
     [SerializeField] private GameObject playableControlsInstructionPanel;
 
+    // Update is called once per frame
     public void Update()
     {
-        if (playableControlsInstructionPanel.activeSelf == true)
+        // Check if the help controls instruction panel is active
+        if (playableControlsInstructionPanel.activeSelf)
         {
+            // Unlock the cursor if the instruction panel is visible
             Cursor.lockState = CursorLockMode.None;
         }
     }
 
+    // Method to be called when continuing from the help controls instruction panel
     public void Continue()
     {
+        // Deactivate the instruction panel
         playableControlsInstructionPanel.SetActive(false);
+        // Resume the game time
         Time.timeScale = 1;
+        // Hide the cursor
         Cursor.visible = false;
     }
 }
