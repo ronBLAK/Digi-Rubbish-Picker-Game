6a929db4dbfcacb8abba46d005e9bfd63e06c14c
ronBLAK <159859853+ronBLAK@users.noreply.github.com>
2024-09-17 19:14:05 +1200

    Update LoseSceneScore.cs

--- Scripts/LoseSceneScore.cs
+++ Scripts/LoseSceneScore.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using TMPro; // Namespace for TextMeshPro components
 
 public class LoseSceneScore : MonoBehaviour
 {
+    // Reference to the TextMeshProUGUI component for displaying the score on the lose screen
     [SerializeField] private TextMeshProUGUI loseScreenScoreText;
 
+    // Update is called once per frame
     public void Update()
     {
+        // Update the score display on the lose screen
         DisplayScore(loseScreenScoreText);
     }
 
+    // Method to set the text of the score display
     private void DisplayScore(TextMeshProUGUI scoreToDisplay)
     {
+        // Format and set the text to show the current score
         scoreToDisplay.text = $"Your Current Score is: {ScoreIncrement.currentScore.ToString()}";
     }
 }
